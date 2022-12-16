3aceb4825416b3ef1c65d7bce64ad249e79b1b0a
adnan <adnanfahedhr@gmail.com>
2022-12-16 19:31:32 +0300

    init items data and list

--- index.js
+++ index.js
@@ -17,26 +17,53 @@ let readPplData = async function () {
     pplData = json;
 }
 
-let render = async function () {
-    var html = '';
-    for (var i = 0; i < pplData.length; i++) {
-        var person = pplData[i];
-        html += `
-            <div class="person" id="${person.id}">
-                <img class="person-img" src="./data/imgs/ppl/${person.id}.webp" />
-                <div class="person-name">${person.name}</div>
-                <div class="person-worth">${person.worth}</div>
-            </div>
-        `;
+let itemsData;
+let readItemsData = async function () {
+    var json = await readJsonFile('./data/items.json');
+    itemsData = json;
+}
+
+
+let render = function () {
+    let renderPpl = function () {
+        var html = '';
+        for (var i = 0; i < pplData.length; i++) {
+            var person = pplData[i];
+            html += `
+                <div class="person" id="${person.id}">
+                    <img class="person-img" src="./data/imgs/ppl/${person.id}.webp" />
+                    <div class="person-name">${person.name}</div>
+                    <div class="person-worth">${person.worth}</div>
+                </div>
+            `;
+        }
+        ppllist.innerHTML = html;
     }
 
-    ppllist.innerHTML = html;
+    let renderItems = function () {
+        var html = '';
+        for (var i = 0; i < itemsData.length; i++) {
+            var item = itemsData[i];
+            html += `
+                <div class="item" id="${item.id}">
+                    <img class="item-img" src="./data/imgs/items/${item.id}.${item.extension}" />
+                    <div class="item-name">${item.name}</div>
+                    <div class="item-price">${item.price}</div>
+                </div>
+            `;
+        }
+        itemslist.innerHTML = html;
+    }
+
+    renderPpl();
+    renderItems();
 }
 
 init = async function () {
     await readPplData();
+    await readItemsData();
 
-    await render();
+    render();
 }
 
 init();
